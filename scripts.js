eadb2a64d61f69c596b3e4420e4d577dcd0d293e
tagaliii <polarbeayr0@gmail.com>
2026-09-18 04:09:55 +0100

    a

--- scripts.js
+++ scripts.js
@@ -1,2 +1,20 @@
-// var audio = new Audio('website.wav');
-// audio.play();
+function test() {
+    document.getElementById("hobson").remove()
+
+    const heading = document.createElement("h1");
+    const image = new Image(500, 500)
+
+    const headingText = document.createTextNode("website");
+
+    
+    heading.appendChild(headingText);
+    
+    document.body.appendChild(heading);
+    document.body.appendChild(image);       
+    document.title = "website";
+    image.src = "images/connectedcog.png"
+
+    var audio = new Audio('website.wav');
+    audio.play();
+    audio.loop = true;
+}
