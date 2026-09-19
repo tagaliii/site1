a53062d1e665e1031e0b3ed104d82cbbac0c5f3d
tagaliii <polarbeayr0@gmail.com>
2026-09-19 04:15:13 +0100

    a2

--- scripts.js
+++ scripts.js
@@ -2,18 +2,25 @@ function forced() {
     document.getElementById("hobson").remove()
 
     const heading = document.createElement("h1");
+    const anchor = document.createElement("a");
     
     const image = new Image(500, 500)
-    image.src = "images/connectedcog.png"
+    image.src = "images/connectedcogtest.png"
     
     const headingText = document.createTextNode("website");
-
+    const anchorText = document.createTextNode("infection");
     
     heading.appendChild(headingText);
+    anchor.appendChild(anchorText);
     
+    document.title = "website";
+    anchor.href = "infection/infection.html"
+
     document.body.appendChild(heading);
     document.body.appendChild(image);       
-    document.title = "website";
+    document.body.appendChild(anchor);
+    document.body.appendChild(anchor);
+    
 
     var audio = new Audio('website.wav');
     audio.play();
